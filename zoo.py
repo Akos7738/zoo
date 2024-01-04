24476042947d68e453bf123911506936bf7e1eff
czmorekakos <czmorekakos@gmail.com>
2024-01-04 13:23:55 +0100

    half good

--- zoo.py
+++ zoo.py
@@ -1,21 +1,26 @@
+def addAnimal(myName):
+    animal = dict()
+    found = False
+    for a in zoo:
+        if myName in a.keys():
+            a[myName] += 1
+            found = True
+    if not found:
+        animal[myName] = 1
+        zoo.append(animal)
+
 print("Ez egy állatkert.")
 print("Állat hozzáadása(1) - Állat törlése(2) - Kilépés(0)")
 
 select = None
 zoo = []
+animal =dict()
 
 while select != "0":
     select = input("Mit szeretne tenni? ")
     if select != "0":
         if select == "1":
             name = input("Az állat neve: ")
-            if name not in animal.keys():
-                animal[name] = 1
-                zoo.append(animal)
-            else:
-                for a in zoo:
-                    a[name] += 1
-            animal = dict()
-            
-for a in zoo:
-    print(a)
+            addAnimal(name)
+
+print(f"Második: {zoo}")
